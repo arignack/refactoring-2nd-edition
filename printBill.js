138e37a026e31492cc1343808065b1545d09e7d2
Alián Rigñack Quevedo <arignack@gmail.com>
2023-03-28 16:37:29 -0400

    Refactors the volumeCreditsFor method

--- printBill.js
+++ printBill.js
@@ -11,16 +11,17 @@ function statement(invoice, plays) {
 
 
     for (let perf of invoice[0].performances) {
-        
-        let thisAmount = 0;
 
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
+
         if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+
         // print line for this order
         result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
+    
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
