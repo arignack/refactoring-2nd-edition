1fd826dd0ab6f6fed59cd935eb83b44930870541
Alián Rigñack Quevedo <arignack@gmail.com>
2023-03-28 16:48:17 -0400

    Splits loops and slide statements

--- printBill.js
+++ printBill.js
@@ -1,29 +1,37 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
+
     let result = `Statement for ${invoice[0].customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {
-            style: "currency",
-            currency: "USD",
-            minimumFractionDigits: 2
-        }).format;
-
-
     for (let perf of invoice[0].performances) {
-
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-
         // print line for this order
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+
+    let volumeCredits = 0;
+    for (let perf of invoice[0].performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+
+    result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
+    return result;
+}
+
+
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber);
+}
+
+function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
     return result;
 }
 
