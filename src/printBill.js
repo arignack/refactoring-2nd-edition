65a5418ab3f993bcaf416ae781dfba79bb8b27e9
Alián Rigñack Quevedo <arignack@gmail.com>
2023-03-29 05:45:05 -0400

    Testing linter configuration

--- src/printBill.js
+++ src/printBill.js
@@ -36,6 +36,7 @@ function volumeCreditsFor(aPerformance) {
 	result += Math.max(aPerformance.audience - 30, 0);
 	if ("comedy" === playFor(aPerformance).type)
 		result += Math.floor(aPerformance.audience / 5);
+	let b = 0;
 	return result;
 }
 
