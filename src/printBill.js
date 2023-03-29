7c739c06226f4d1efc2205c321d39538b78cd59e
Alián Rigñack Quevedo <arignack@gmail.com>
2023-03-29 16:41:26 -0400

    Renames internal variables for a better semantic

--- src/printBill.js
+++ src/printBill.js
@@ -6,24 +6,24 @@ function statement(invoice, plays) {
 		} seats)\n`;
 	}
 
-	result += `Amount owed is ${usd(appleSauce())}\n`;
+	result += `Amount owed is ${usd(totalAmount())}\n`;
 	result += `You earned ${totalVolumeCredits()} credits\n`;
 	return result;
 
 	function totalVolumeCredits() {
-		let volumeCredits = 0;
+		let result = 0;
 		for (let perf of invoice[0].performances) {
-			volumeCredits += volumeCreditsFor(perf);
+			result += volumeCreditsFor(perf);
 		}
-		return volumeCredits;
+		return result;
 	}
 
-	function appleSauce() {
-		let totalAmount = 0;
+	function totalAmount() {
+		let result = 0;
 		for (let perf of invoice[0].performances) {
-			totalAmount += amountFor(perf);
+			result += amountFor(perf);
 		}
-		return totalAmount;
+		return result;
 	}
 }
 
