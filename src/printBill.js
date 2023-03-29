343fde7fa15004c33744d9260a23c07f2bae2a2c
Alián Rigñack Quevedo <arignack@gmail.com>
2023-03-29 05:45:52 -0400

    Fixes the test

--- src/printBill.js
+++ src/printBill.js
@@ -36,7 +36,6 @@ function volumeCreditsFor(aPerformance) {
 	result += Math.max(aPerformance.audience - 30, 0);
 	if ("comedy" === playFor(aPerformance).type)
 		result += Math.floor(aPerformance.audience / 5);
-	let b = 0;
 	return result;
 }
 
