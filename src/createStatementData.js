9279be9bc0ff7bfe6895d5908770914bdd8632c9
Alián Rigñack Quevedo <arignack@gmail.com>
2023-04-10 16:13:17 -0400

    Refactors the amontFor function to use the PerformanceCalculator

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -3,6 +3,28 @@ class PerformanceCalculator {
 		this.performance = aPerformance;
 		this.play = aPlay;
 	}
+
+	get amount() {
+		let result = 0;
+		switch (this.play.type) {
+			case "tragedy":
+				result = 40000;
+				if (this.performance.audience > 30) {
+					result += 1000 * (this.performance.audience - 30);
+				}
+				break;
+			case "comedy":
+				result = 30000;
+				if (this.performance.audience > 20) {
+					result += 10000 + 500 * (this.performance.audience - 20);
+				}
+				result += 300 * this.performance.audience;
+				break;
+			default:
+				throw new Error(`unknown type: ${this.play.type}`);
+		}
+		return result;
+	}
 }
 
 export default function createStatementData(invoice, plays) {
@@ -20,31 +42,13 @@ export default function createStatementData(invoice, plays) {
 		);
 		const result = Object.assign({}, aPerformance);
 		result.play = calculator.play;
-		result.amount = amountFor(result);
+		result.amount = calculator.amount;
 		result.volumeCredits = volumeCreditsFor(result);
 		return result;
 	}
 
 	function amountFor(perf) {
-		let thisAmount = 0;
-		switch (perf.play.type) {
-			case "tragedy":
-				thisAmount = 40000;
-				if (perf.audience > 30) {
-					thisAmount += 1000 * (perf.audience - 30);
-				}
-				break;
-			case "comedy":
-				thisAmount = 30000;
-				if (perf.audience > 20) {
-					thisAmount += 10000 + 500 * (perf.audience - 20);
-				}
-				thisAmount += 300 * perf.audience;
-				break;
-			default:
-				throw new Error(`unknown type: ${perf.play.type}`);
-		}
-		return thisAmount;
+		return new PerformanceCalculator(perf, playFor(perf).amount);
 	}
 
 	function playFor(aPerformance) {
